f261c5c94de074c686e7563c61b2dcfdd3079874
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2024-04-20 21:00:44 +0800

    [IO]add clang-format file

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,8 @@
+#include <iostream>
+#include <omp.h>
 #include <stdio.h>
 #include <string>
-#include <iostream>
 #include <unistd.h>
-#include <omp.h>
 
 using namespace std;
 
@@ -13,30 +13,30 @@ struct Config {
     bool flagB = false;
 };
 
-struct Config get_config(int argc, char** argv) {
+struct Config get_config(int argc, char **argv) {
     int opt;
     struct Config c;
-    
-    while ( (opt = getopt(argc, argv, "p:m:")) != -1 ) { 
-        switch ( opt ) {
-            case 'p':
-                    c.path = optarg;
-                    printf("input graph path: %s\n", c.path.c_str());
-                break;
-            case 'm':
-                    c.method = string(optarg);
-                    printf("method: %s\n", c.path.c_str());
-                break;
-            case '?':  // unknown option...
-                    cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
-                break;
+
+    while ((opt = getopt(argc, argv, "p:m:")) != -1) {
+        switch (opt) {
+        case 'p':
+            c.path = optarg;
+            printf("input graph path: %s\n", c.path.c_str());
+            break;
+        case 'm':
+            c.method = string(optarg);
+            printf("method: %s\n", c.path.c_str());
+            break;
+        case '?': // unknown option...
+            cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
+            break;
         }
     }
 
     return c;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     get_config(argc, argv);
 
     return 0;
