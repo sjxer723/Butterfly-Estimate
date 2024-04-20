4d5cba72f874b430b46597f79e15aeaa3687cb7f
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2024-04-20 20:59:12 +0800

    [BTF]add command line parser

---
+++ src/main.cc
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <omp.h>
+
+using namespace std;
+
+struct Config {
+    string path;
+    string method;
+    bool flagA = false;
+    bool flagB = false;
+};
+
+struct Config get_config(int argc, char** argv) {
+    int opt;
+    struct Config c;
+    
+    while ( (opt = getopt(argc, argv, "p:m:")) != -1 ) { 
+        switch ( opt ) {
+            case 'p':
+                    c.path = optarg;
+                    printf("input graph path: %s\n", c.path.c_str());
+                break;
+            case 'm':
+                    c.method = string(optarg);
+                    printf("method: %s\n", c.path.c_str());
+                break;
+            case '?':  // unknown option...
+                    cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
+                break;
+        }
+    }
+
+    return c;
+}
+
+int main(int argc, char** argv) {
+    get_config(argc, argv);
+
+    return 0;
+}
